4ea2ee74e94dd66591e6c508ad06bf52bd495b7c
JairoDuenas <jairoduenas.ing@gmail.com>
2022-07-28 08:30:49 -0500

    State

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,11 @@
 import { createStore } from "vuex";
 
-const store = createStore({});
+const store = createStore({
+  state() {
+    return {
+      username: "Diana.Nerd",
+    };
+  },
+});
 
 export default store;
