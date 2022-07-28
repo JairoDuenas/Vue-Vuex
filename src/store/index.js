9a22a8d55b040ae29c4ef01ee069fe84fbffdf21
JairoDuenas <jairoduenas.ing@gmail.com>
2022-07-28 08:46:15 -0500

    Getters

--- src/store/index.js
+++ src/store/index.js
@@ -6,6 +6,11 @@ const store = createStore({
       username: "Diana.Nerd",
     };
   },
+  getters: {
+    firstName: (state) => (c) => {
+      return state.username.split(".").reverse().join(c);
+    },
+  },
 });
 
 export default store;
